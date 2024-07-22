59caa50ba5e89a005a3d2d0907dae10f65430b98
Mauricio <maurytrossero@gmail.com>
2024-07-22 19:45:11 -0300

    api-conexion-TOKEN-fix8

--- src/components/AuthCallback.vue
+++ src/components/AuthCallback.vue
@@ -1,24 +1,25 @@
 <template>
     <div>
-      <p>Autorización en curso...</p>
+      <p>Token de acceso: {{ accessToken }}</p>
+      <p>User ID: {{ userId }}</p>
     </div>
   </template>
   
   <script lang="ts" setup>
-  import { onMounted } from 'vue';
+  import { ref, onMounted } from 'vue';
   import axios from 'axios';
   import { useRoute, useRouter } from 'vue-router';
   
   const route = useRoute();
   const router = useRouter();
+  const accessToken = ref('');
+  const userId = ref('');
   
   onMounted(async () => {
     const code = route.query.code as string;
   
     if (code) {
       try {
-        console.log('Código recibido:', code); // Verifica que se recibe el código
-  
         const response = await axios.post('https://api.instagram.com/oauth/access_token', {
           client_id: '1246190323410382',
           client_secret: 'b0c222b5f2e29a493a573709a3423470',
@@ -27,20 +28,16 @@
           code: code
         });
   
-        console.log('Respuesta de la API de Instagram:', response.data);
-  
         const { access_token, user_id } = response.data;
+        accessToken.value = access_token;
+        userId.value = user_id;
+  
         console.log('Token de acceso:', access_token);
         console.log('User ID:', user_id);
   
-        // Verifica si los valores existen antes de almacenarlos
-        if (access_token && user_id) {
-          localStorage.setItem('instagram_access_token', access_token);
-          localStorage.setItem('instagram_user_id', user_id);
-          console.log('Token y User ID almacenados en localStorage.');
-        } else {
-          console.error('Token de acceso o User ID faltante en la respuesta.');
-        }
+        // Aquí es donde guardarías en localStorage normalmente
+        // localStorage.setItem('instagram_access_token', access_token);
+        // localStorage.setItem('instagram_user_id', user_id);
   
         router.push('/');
       } catch (error) {
