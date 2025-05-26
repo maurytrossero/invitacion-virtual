1468be1783ec5c04eff0461de17efa2ecaa4d749
Mauricio <maurytrossero@gmail.com>
2025-05-26 17:35:44 -0300

    componente informacion modales agregada1

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: './', // Rutas relativas para CSS y JS
+  css: {
+    extract: true, // Extraer CSS a archivos separados (por defecto true)
+  },
+  configureWebpack: {
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          styles: {
+            name: 'styles',
+            test: /\.(css|vue)$/,
+            chunks: 'all',
+            enforce: true,
+          },
+        },
+      },
+    },
+  },
 })
